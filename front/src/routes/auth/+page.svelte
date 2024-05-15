c5654926280fc4c281af0289dda97951aa9d95e6
Florian <f0208r@gmail.com>
2024-05-15 18:23:54 +0200

    [ADD] youtube and spotify player

---
+++ front/src/routes/auth/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    onMount(() => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const token = urlParams.get('access_token');
+        if (token) {
+            localStorage.setItem('token', token);
+        }
+    });
+</script>
---
+++ front/src/routes/home/spotify/[id]/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let data;
+
+    let MusicId = data.id
+</script>
+
+<main class="h-full">
+    <iframe 
+        src={`https://open.spotify.com/embed/track/${MusicId}`} 
+        title="Spotify music player"
+        class="w-full h-4/6"
+        frameborder="0" 
+        allow="encrypted-media">
+    </iframe>
+    <div class="flex justify-around items-center h-2/6 variant-glass-primary">
+        <button class="btn variant-filled m-4">
+            <img src="/like.svg" alt="like" class="btn-icon">
+        </button>
+        <button class="btn variant-filled m-4">
+            <img src="/favorite.svg" alt="favorite" class="btn-icon">
+        </button>
+    </div>
+</main>

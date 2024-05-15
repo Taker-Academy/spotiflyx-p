196116bdd9a3ad4724f0933f63e4200efbbd3faa
Florian <f0208r@gmail.com>
2024-05-15 13:28:37 +0200

    [ADD] last upload, search and favorite section

--- front/src/routes/watch/+layout.svelte
+++ front/src/routes/watch/+layout.svelte
@@ -1,26 +1,20 @@
 <script>
     import "../../app.css";
     import { AppBar } from '@skeletonlabs/skeleton';
-    import { searchQuery } from './store.js';
 </script>
 
 <AppBar class="custom-appbar">
     <svelte:fragment slot="lead">
         <a href="/">
-            <img src="/logo.png" alt="Your Icon" class="h-16 w-16 ml-8"/>
+            <img src="/logo.png" alt="Spotiflix logo" class="big-icon ml-8"/>
         </a>
     </svelte:fragment>
-    <div class="input-group input-group-divider grid-cols-[auto_1fr_auto] h-12">
-        <div class="input-group-shim"><img src="/search_icon.svg" alt="Search Icon" class=""/></div>
-        <input type="search" bind:value={$searchQuery} placeholder="Search..." class="pl-1" />
-        <button class="variant-filled-secondary">Submit</button>
-    </div>
     <svelte:fragment slot="trail">
         <a href="/add">
-            <img src="/add.svg" alt="Your Icon" class="h-16 w-16"/>
+            <img src="/add.svg" alt="add" class="big-icon"/>
         </a>
         <a href="/user">
-            <img src="/user.svg" alt="Your Icon" class="h-16 w-16 mr-8"/>
+            <img src="/user.svg" alt="user" class="big-icon mr-8"/>
         </a>
     </svelte:fragment>
 </AppBar>
--- front/src/routes/watch/+page.svelte
+++ front/src/routes/watch/+page.svelte
@@ -1,37 +1,29 @@
-<script type="ts">
-    import "../../app.css";
-    import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
-    import { searchQuery } from './store.js';
-    import SearchResult from '$lib/SearchResult.svelte';
+<script lang="ts">
+	import LastUpload from './../../lib/LastUpload.svelte';
+	import Search from '../../lib/Search.svelte';
+	import Favorite from './../../lib/Favorite.svelte';
+	import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
 
-    let tabSet = 0;
-    let searchValue = '';
-
-    searchQuery.subscribe(value => {
-        searchValue = value;
-    });
+	let tabSet: number = 0;
 </script>
 
-<TabGroup
-    justify="justify-center"
-	active="variant-filled-primary"
-	hover="hover:variant-soft-primary"
-	flex="flex-1 lg:flex-none"
-	rounded=""
-	class="bg-surface-100-800-token w-full"
->
-	<Tab bind:group={tabSet} name="tab1" value={0}>
-        <img src="/youtube.svg" alt="YouTube" class="search-icon"/>
-    </Tab>
-	<Tab bind:group={tabSet} name="tab2" value={1}>
-        <img src="/spotify.svg" alt="Spotify" class="search-icon"/>
-    </Tab>
+
+
+<TabGroup justify="justify-center" class="bg-surface-400">
+	<Tab bind:group={tabSet} name="tab1" value={0}><img src="/home.svg" alt="home" class="icon"/></Tab>
+	<Tab bind:group={tabSet} name="tab2" value={1}><img src="/search_icon.svg" alt="search" class="icon"/></Tab>
+	<Tab bind:group={tabSet} name="tab3" value={2}><img src="/favorite.svg" alt="Favorite" class="icon"/></Tab>
 	<svelte:fragment slot="panel">
 		{#if tabSet === 0}
-			<SearchResult {searchValue}></SearchResult>
+			<LastUpload></LastUpload>
 		{:else if tabSet === 1}
-            <SearchResult {searchValue}></SearchResult>
+			<Search></Search>
+		{:else if tabSet === 2}
+			<Favorite></Favorite>
 		{/if}
 	</svelte:fragment>
 </TabGroup>
+			
+
+
 			

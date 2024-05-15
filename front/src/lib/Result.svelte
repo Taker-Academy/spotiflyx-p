73e117ba447a378157f3f368890b6628378d15e8
Florian <f0208r@gmail.com>
2024-05-15 17:04:17 +0200

    [ADD] console log when pressing buttons

--- front/src/lib/Result.svelte
+++ front/src/lib/Result.svelte
@@ -8,12 +8,8 @@
 <div class="max-h-screen overflow-x-auto h-full min-h-screen  pb-48">
     {#each range as number}
         <div class="block card card-hover p-4 m-4 mx-auto h-80 w-96 bg-surface-400">
-            <img src="https://img.youtube.com/vi/GtffucAmM78/maxresdefault.jpg" alt="video thumbnail" class="w-full rounded-lg">
+            <img src="https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg" alt="video thumbnail" class="w-full rounded-lg">
             <p>{searchValue}</p>
         </div>
     {/each}
 </div>
-
-<style>
-
-</style>
--- front/src/routes/register/+page.svelte
+++ front/src/routes/register/+page.svelte
@@ -2,7 +2,16 @@
     import { goto } from "$app/navigation";
     import "../../app.css";
 
+    let user = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+    }
+
     function register() {
+        const userData = JSON.stringify(user);
+        console.log(userData);
         goto("/home");
     }
 </script>
@@ -11,10 +20,10 @@
     <div class="glass-form">
         <div class="w-full">
             <h2 class="text-7xl mb-12">Register</h2>
-            <input class="input mb-4 pl-3" title="Input (text)" type="text" placeholder="First Name" />
-            <input class="input mb-4 pl-3" title="Input (text)" type="text" placeholder="Last Name" />
-            <input class="input mb-4 pl-3" title="Input (email)" type="email" placeholder="john@example.com" autocomplete="email" />
-            <input class="input mb-4 pl-3" title="Input (password)" type="password" placeholder="password" />
+            <input class="input mb-4 pl-3" bind:value={user.firstName} title="Input (text)" type="text" placeholder="First Name" />
+            <input class="input mb-4 pl-3" bind:value={user.lastName} title="Input (text)" type="text" placeholder="Last Name" />
+            <input class="input mb-4 pl-3" bind:value={user.email} title="Input (email)" type="email" placeholder="john@example.com" autocomplete="email" />
+            <input class="input mb-4 pl-3" bind:value={user.password} title="Input (password)" type="password" placeholder="password" />
         </div>
         <div class="flex flex-col">
             <button type="button" on:click={register} class="btn variant-filled m-4">Register</button>

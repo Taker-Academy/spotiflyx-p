d6e2799aea69b02b76e883c280eb9864a8a58516
William Jolivet <william.jolivet@epitech.eu>
2024-05-15 20:17:27 +0200

    [PATCH] get account

--- backend/src/main/java/fr/william/spotiflyx_api/routes/GetAccountRoute.java
+++ backend/src/main/java/fr/william/spotiflyx_api/routes/GetAccountRoute.java
@@ -1,11 +1,25 @@
 package fr.william.spotiflyx_api.routes;
 
+import fr.william.spotiflyx_api.AuthService;
+import fr.william.spotiflyx_api.Main;
 import fr.william.spotiflyx_api.SpotiflyxRoute;
+import fr.william.spotiflyx_api.response.Response;
+import fr.william.spotiflyx_api.token.TokenManager;
 import io.javalin.http.Context;
+import org.bson.Document;
 
 public class GetAccountRoute implements SpotiflyxRoute {
     @Override
     public void handle(Context ctx) {
+        String token = ctx.header("Authorization");
 
+        if (!AuthService.isAuthorized(token)) {
+            ctx.status(401);
+            ctx.result(new Document("ok", false).append("message", "Unauthorized").toJson());
+            return;
+        }
+        Response response = Main.getUserService().getAccountFromToken(AuthService.extractToken(token));
+        ctx.status(response.getStatus());
+        ctx.result(response.toJson());
     }
 }
---
+++ backend/src/main/java/fr/william/spotiflyx_api/routes/content/LikeContentRoute.java
@@ -0,0 +1,11 @@
+package fr.william.spotiflyx_api.routes.content;
+
+import fr.william.spotiflyx_api.SpotiflyxRoute;
+import io.javalin.http.Context;
+
+public class LikeContentRoute implements SpotiflyxRoute {
+    @Override
+    public void handle(Context ctx) {
+
+    }
+}
